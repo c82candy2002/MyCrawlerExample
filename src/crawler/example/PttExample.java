71ba1b64607060bd7c5081ceb37524dc8a8da86c
Candy <c82candy2002@gmail.com>
2017-12-10 12:02:32 +0800

    2017.12.10

--- src/crawler/example/PttExample.java
+++ src/crawler/example/PttExample.java
@@ -2,6 +2,7 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,11 +21,9 @@ public class PttExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "https://www.ptt.cc/bbs/Boy-Girl/M.1512711503.A.BA4.html";
+		String uri = "https://www.ptt.cc/bbs/PokemonGO/M.1512743439.A.4C5.html";
+		Elements jsoup = CrawlerPack.start()
 
-		System.out.println(
-				CrawlerPack.start()
-				
 				// 參數設定推) contains
 			    .addCookie("over18","1")	// 設定cookie
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
@@ -35,8 +34,11 @@ public class PttExample {
 			    //.getFromXml(uri)
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
-			    .select("span.hl.push-tag:contains(推)+span")
+			    .select("#main-container");
 
-		);
+		System.out.println(jsoup.select("div,span").remove().text() );
+
+
+
 	}
 }
